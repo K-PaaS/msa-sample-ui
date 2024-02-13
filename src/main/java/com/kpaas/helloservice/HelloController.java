d88cc166a546d08e066f6eb464a58c709469121e
hoon77 <slokang@naver.com>
2024-02-13 14:26:58 +0900

    init

---
+++ src/main/java/com/kpaas/helloservice/HelloController.java
@@ -0,0 +1,25 @@
+package com.kpaas.helloservice;
+
+import com.kpaas.helloservice.model.ResultData;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class HelloController {
+
+    @Autowired
+    private NameServiceClient nameServiceClient;
+
+    @GetMapping("/hello")
+    public String getHello(Model model) {
+        ResultData info = nameServiceClient.getInfo();
+        model.addAttribute("data1",info.getData1());
+        model.addAttribute("data2",info.getData2());
+        model.addAttribute("data3",info.getData3());
+        model.addAttribute("data4",info.getData4());
+        return "index";
+    }
+}
